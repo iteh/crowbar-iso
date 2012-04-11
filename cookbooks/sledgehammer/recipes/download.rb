4a8ad947b3da267cd00e058d6124947c534b0b90
Edmund Haselwanter <edmund@haselwanter.com>
2012-04-11 18:22:05 +0200

    finish sledgehammer download method

--- cookbooks/sledgehammer/recipes/download.rb
+++ cookbooks/sledgehammer/recipes/download.rb
@@ -3,20 +3,30 @@
 directory node.sledgehammer.build_cache_path do
   recursive true
   action :delete
-end if node.crowbar.prune_build_cache
+end if node[:crowbar] && node.crowbar.prune_build_cache
 
 directory node.sledgehammer.build_cache_path do
   action :create
 end
 
-remote_file "/tmp/sledgehammer-tftpboot.tar.gz" do
-  source default.sledgehammer.download_url
+bash "untar sledgehammer" do
+  code %Q{tar -xzvf /tmp/sledgehammer-tftpboot.tar.gz}
+  cwd node.sledgehammer.build_cache_path
   action :nothing
+  not_if do
+    File.exists?(File.join(node.sledgehammer.build_cache_path,"tftpboot"))
+  end
 end
- 
-http_request "HEAD #{default.sledgehammer.download_url}" do
+
+remote_file "/tmp/sledgehammer-tftpboot.tar.gz" do
+  source node.sledgehammer.download_url
+  action :nothing
+  notifies :run, resources(:bash => "untar sledgehammer"), :immediately
+end
+
+http_request "HEAD #{node.sledgehammer.download_url}" do
   message ""
-  url default.sledgehammer.download_url
+  url node.sledgehammer.download_url
   action :head
   if File.exists?("/tmp/sledgehammer-tftpboot.tar.gz")
     headers "If-Modified-Since" => File.mtime("/tmp/sledgehammer-tftpboot.tar.gz").httpdate
