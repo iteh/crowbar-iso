9a8dc0c6ba1665507d43be5740c2ddeea07cb88e
Edmund Haselwanter <edmund@haselwanter.com>
2012-04-12 18:44:45 +0200

    upgrade all packages (openssl problem)

--- cookbooks/crowbar/recipes/default.rb
+++ cookbooks/crowbar/recipes/default.rb
@@ -1,8 +1,11 @@
 #https://github.com/dellcloudedge/crowbar/wiki/Build-Crowbar.ISO
 
-%W{git genisoimage build-essential debootstrap mkisofs binutils ruby curl}.each do |package_name|
-  package package_name
+%W{openssl git genisoimage build-essential debootstrap mkisofs binutils ruby curl}.each do |package_name|
+  package package_name do
+    action :upgrade
+  end
 end 
+
 crowbar_repo_local_uri = "/root/crowbar"
 
 git crowbar_repo_local_uri do
